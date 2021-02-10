42aa9225442dd2b60b14102421068d255bd2b0b9
Hazim Arafa <hazimarafa80@gmail.com>
2021-02-09 22:24:28 -0800

    Create discordstatus.py

---
+++ discordstatus.py
@@ -0,0 +1,58 @@
+import keyboard
+from pynput.mouse import Button, Controller
+import json
+import requests
+from time import sleep
+from googletrans import Translator
+
+translator = Translator(service_urls=[
+    'translate.google.com',
+])
+
+api_url = 'https://uselessfacts.jsph.pl/today.json'
+
+def fetch():
+    response = requests.get(api_url)
+    response = json.loads(response.text)
+
+    return response
+
+response = fetch()
+
+if response['language'] != 'en':
+    response = translator.translate(
+        response['text'],
+        src=response['language']
+    )
+
+useless_fact = response.text
+new_status = f'Fact: {useless_fact} This status Changes every day'
+
+##########################################################################
+
+mouse = Controller()
+
+pressed = False
+
+pfp = (-1850, 1150)
+status = (-1850, 1100)
+clear = (-822, 608)
+
+def press_in_a_certian_place(position):
+    mouse.position = (position)
+    sleep(0.1)
+    mouse.press(Button.left)
+    mouse.release(Button.left)
+
+def set_new_status():
+    press_in_a_certian_place(pfp)
+    press_in_a_certian_place(pfp)
+    sleep(0.15)
+    press_in_a_certian_place(status)
+    sleep(0.15)
+    press_in_a_certian_place(clear)
+    press_in_a_certian_place(clear)
+    keyboard.write(new_status)
+    keyboard.press_and_release('enter')
+
+set_new_status()
